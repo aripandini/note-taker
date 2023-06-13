053968edb71e19f5dfb2ba8dbf4b36befb28a8db
aripandini <ariannepandini@hotmail.com>
2023-06-12 23:38:18 -0400

    small progress, but still getting errors when showing notes

--- Develop/routes/routes.js
+++ Develop/routes/routes.js
@@ -1,17 +1,40 @@
 const express = require('express');
-const fs = require('fs');
-// const { v4: uuidv4 } = require('uuid'); //To generate unique ids
+const db = require('../db/db.json');
+const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+const { v4: uuidv4 } = require('uuid'); //To generate unique ids
 
 const app = express();
 
 
 //Open Notes
-app.get('/api/notes', (req, res) => {
-  readFromFile('./db/db.json').then((data) =>
+app.get('api/notes', (req, res) => {
+  readFromFile('../db/db.json').then((data) =>
   res.json(data)
   );
 });
 
+//New Note
+app.post('api/notes', (req, res) => {
+  const { title, text } = req.body;
+
+  if(title && text) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4()
+    };
+    const response = {
+      body: newNote
+    };
+
+    db.push(newNote);
+    readAndAppend(newNote, './db/db.json');
+
+    res.json(response);
+  } else {
+    res.json('Error creating a new note');
+  }
+});
 
 
 module.exports = app;
